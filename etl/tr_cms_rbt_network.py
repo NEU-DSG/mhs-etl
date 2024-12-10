5422f907093be237f26b66a4115e59368be993ef
joelsjlee <joelsjlee@gmail.com>
2024-12-10 15:11:13 -0500

    edit cms rbt network to omit the source and target additions. It was causing too many issues regarding multiple sources and targets and no sources and targets. I also think adding source and target as co-references is a bit tenuous.

--- etl/tr_cms_rbt_network.py
+++ etl/tr_cms_rbt_network.py
@@ -14,7 +14,7 @@ def create_dataframe(files, log):
     file and creates our pandas dataframe.
     '''
     df = build_dataframe(files)
-    df.to_csv("debug/build.csv")
+    # df.to_csv("debug/build.csv")
     print("Number of Entries: "  + str(len(df)))
     print("Average Length of Entry: " + str(df['text'].apply(len).mean()))
     log['num_entries'] = str(len(df))
@@ -28,9 +28,10 @@ def create_dataframe(files, log):
     # Split references into list objects.
     df['references'] = df['references'].str.split(r',|;')
     # df['target'] = df['target'].str.split(r',|;')
-    df.to_csv("debug/split.csv")
+    # df.to_csv("debug/split.csv")
     print("Average Num of People Mentioned per Entry " + str(df['references'].apply(len).mean()))
     log['pers_ref_avg'] = str(df['references'].apply(len).mean())
+    print("length of df after create dataframe", df.shape)
 
     return df
 
@@ -38,52 +39,75 @@ def create_adj_matrix(df, weight):
     ''' Creates the adjacency matrix from the dataframe '''
     # Explode list so that each list value becomes a row.
     refs = df.explode('references')
+    print("length of df after explosion", refs.shape)
     # refs = refs.explode('target')
     refs['source'] = refs['source'].str.strip()
-    # refs['target'] = refs['target'].str.strip()
+    refs['target'] = refs['target'].str.strip()
     refs['references'] = refs['references'].str.strip()
-    refs.to_csv("explode.csv")
+    # refs.to_csv("explode.csv")
 
     # Create file-person matrix.
     refs = pd.crosstab(refs['file'], refs['references'])
-    refs.to_csv("debug/adj.csv")
+    print("length of df after crosstab", refs.shape)
+    # refs.to_csv("debug/adj.csv")
 
     # Repeat with correspondence (source + target)
-    source = pd.crosstab(df['file'], df['source'])
-    # df_target = df.explode('target')
+    # source = pd.crosstab(df['file'], df['source'])
+    # source.to_csv("debug/source.csv")
+
+    # print("length of source after crosstab", source.shape)
+    # print(source['file'])
+    '''
+    We need to make a copy of the df to make a separate df_target dataframe
+    for letters in which there are multiple targets/recipients. later in the 
+    code we add targets to the columns of the refs, either adding the column
+    value to the existing column (if the recipient was also a reference), or 
+    adding the column itself otherwise. The issue here is if a target was only
+    a target in a letter with multiple targets. That means that when we process 
+    the pd.crosstab for the targets, there is no singular target column to add
+    to the refs column.
+    '''
+    
+    # df_target = df.copy(deep=True)
+    # df_target['target'] = df_target['target'].str.split(r',|;')
+    # df_target.to_csv("debug/df_target.csv")
+    # df_target = df_target.explode('target')
     # df_target['target'] = df_target['target'].str.strip()
+    
     # target = pd.crosstab(df_target['file'], df_target['target'])
-    target = pd.crosstab(df['file'], df['target'])
-    target.to_csv("target.csv")
+    # # target = pd.crosstab(df['file'], df['target'])
+    # target.to_csv("debug/target.csv")
 
     # Sum values of sources to refs or create new column with sources' values.
-    for col in source:
-        if col in refs:
-            refs[str(col)] = refs[str(col)] + source[str(col)]
-        else:
-            refs[str(col)] = source[str(col)]
+    # print("length of refs: ", len(refs))
+    # print("length of source: ", len(source))
+    # for col in source:
+    #     if col in refs:
+    #         refs[str(col)] = refs[str(col)] + source[str(col)]
+    #     else:
+    #         refs[str(col)] = source[str(col)]
 
     # Repeat for targets.
-    for col in target:
-        if ';' in col:
-            print("HEEEREEE + ", col)
-            cols = [item.strip() for item in col.split(';')]
-            print(cols)
-            for columns in cols:
-                print("NEW", columns)
-                if columns in refs:
-                    refs[str(columns)] = refs[str(columns)] + target[str(columns)]
-                else:
-                    refs[str(columns)] = target[str(columns)]
-        else:
-            if col in refs:
-                refs[str(col)] = refs[str(col)] + target[str(col)]
-            else:
-                refs[str(col)] = target[str(col)]
+    # refs.to_csv("debug/before.csv")
+    # for col in target:
+    #     if ';' in col:
+    #         cols = [item.strip() for item in col.split(';')]
+    #         for columns in cols:
+    #             if columns in refs:
+    #                 refs[str(columns)] = refs[str(columns)] + target[str(columns)]
+    #             else:
+    #                 refs[str(columns)] = target[str(columns)]
+    #     else:
+    #         if col in refs:
+    #             refs[str(col)] = refs[str(col)] + target[str(col)]
+    #         else:
+    #             refs[str(col)] = target[str(col)]
+
+    # refs.to_csv("debug/after.csv")
 
     # Convert entry-person matrix into an adjacency matrix of persons.
     refs = refs.T.dot(refs)
-    refs.to_csv("debug/dot.csv")
+    # refs.to_csv("debug/dot.csv")
 
     # # Change diagonal values to zero. That is, a person cannot co-occur with themself.
     np.fill_diagonal(refs.values, 0)
